9b674fe21b42dc7bc16a3ef9128852785f810e75
widianapw <widianaputraa@gmail.com>
2023-07-24 15:11:34 +0800

    test husky

--- tmd/index.tsx
+++ tmd/index.tsx
@@ -1,64 +1,64 @@
-import * as Colors from "./styles/defaultThemeColors";
+import * as Colors from './styles/defaultThemeColors';
 
-export { Colors };
+export {Colors};
 
-export { appTheme, withTheme, ThemeProvider } from "./core/theming";
+export {appTheme, withTheme, ThemeProvider} from './core/theming';
 //provider
-export { default as Provider } from "./core/Provider";
-export { default as BottomSheetProvider } from "./providers/BottomSheetProvider";
-export { default as ModalProvider } from "./providers/ModalProvider";
-export { default as PermissionProvider } from "./providers/PermissionProvider";
-export { default as TooltipStepperProvider } from "./providers/TooltipStepperProvider";
+export {default as Provider} from './core/Provider';
+export {default as BottomSheetProvider} from './providers/BottomSheetProvider';
+export {default as ModalProvider} from './providers/ModalProvider';
+export {default as PermissionProvider} from './providers/PermissionProvider';
+export {default as TooltipStepperProvider} from './providers/TooltipStepperProvider';
 
-export { default as DefaultTheme } from "./styles/theme/DefaultTheme";
-export { default as shadow } from "./styles/shadow";
-export { default as overlay } from "./styles/overlay";
-export { default as configureFonts } from "./styles/fonts";
+export {default as DefaultTheme} from './styles/theme/DefaultTheme';
+export {default as shadow} from './styles/shadow';
+export {default as overlay} from './styles/overlay';
+export {default as configureFonts} from './styles/fonts';
 
-export { default as Alert } from "./components/Alert/Alert";
-export { default as Badge } from "./components/Badge/Badge";
-export { default as Tag } from "./components/Tag/Tag";
-export { default as Banner } from "./components/Banner";
-export { default as Button } from "./components/Button/Button";
-export { default as Checkbox } from "./components/Checkbox";
-export { default as Divider } from "./components/Divider";
-export { default as HelperText } from "./components/HelperText";
-export { default as IconButton } from "./components/IconButton";
-export { default as RadioButton } from "./components/RadioButton";
-export { default as Snackbar } from "./components/Snackbar";
-export { default as Surface } from "./components/Surface";
-export { default as Switch } from "./components/Switch/Switch";
-export { default as TouchableRipple } from "./components/TouchableRipple/TouchableRipple";
-export { default as TextField } from "./components/TextInput/TextField";
-export { default as Icon } from "./components/Icon";
-export { default as RHFTextField } from "./components/RHF/RHFTextField";
-export { default as RHFSelect } from "./components/RHF/RHFSelect";
-export { default as RHFPhoneField } from "./components/RHF/RHFPhoneField";
-export { default as RHFTimePicker } from "./components/RHF/RHFTimePicker";
-export { default as RHFDatePicker } from "./components/RHF/RHFDatePicker";
+export {default as Alert} from './components/Alert/Alert';
+export {default as Badge} from './components/Badge/Badge';
+export {default as Tag} from './components/Tag/Tag';
+export {default as Banner} from './components/Banner';
+export {default as Button} from './components/Button/Button';
+export {default as Checkbox} from './components/Checkbox';
+export {default as Divider} from './components/Divider';
+export {default as HelperText} from './components/HelperText';
+export {default as IconButton} from './components/IconButton';
+export {default as RadioButton} from './components/RadioButton';
+export {default as Snackbar} from './components/Snackbar';
+export {default as Surface} from './components/Surface';
+export {default as Switch} from './components/Switch/Switch';
+export {default as TouchableRipple} from './components/TouchableRipple/TouchableRipple';
+export {default as TextField} from './components/TextInput/TextField';
+export {default as Icon} from './components/Icon';
+export {default as RHFTextField} from './components/RHF/RHFTextField';
+export {default as RHFSelect} from './components/RHF/RHFSelect';
+export {default as RHFPhoneField} from './components/RHF/RHFPhoneField';
+export {default as RHFTimePicker} from './components/RHF/RHFTimePicker';
+export {default as RHFDatePicker} from './components/RHF/RHFDatePicker';
 
-export { default as Stack } from "./components/Layout/Stack";
-export { default as Page } from "./components/Page";
-export { default as Grid } from "./components/Layout/Grid";
-export { default as Toast } from "./components/Toast";
-export { default as GridList } from "./components/FlatList/GridList";
-export { default as Skeleton } from "./components/Skeleton/Skeleton";
-export { default as ImageSlider } from "./components/Image/GallerySlider";
-export { default as ImageZoom } from "./components/Image/ImageZoom";
+export {default as Stack} from './components/Layout/Stack';
+export {default as Page} from './components/Page';
+export {default as Grid} from './components/Layout/Grid';
+export {default as Toast} from './components/Toast';
+export {default as GridList} from './components/FlatList/GridList';
+export {default as Skeleton} from './components/Skeleton/Skeleton';
+export {default as ImageSlider} from './components/Image/GallerySlider';
+export {default as ImageZoom} from './components/Image/ImageZoom';
 
 //progress
-export { default as CircularProgressBar } from "./components/ProgressBar/CircularProgressBar";
-export { default as LinearProgressBar } from "./components/ProgressBar/LinearProgressBar";
+export {default as CircularProgressBar} from './components/ProgressBar/CircularProgressBar';
+export {default as LinearProgressBar} from './components/ProgressBar/LinearProgressBar';
 
 //tooltip
-export { default as Tooltip } from "./components/Tooltip/Tooltip";
-export { default as TooltipStepper } from "./components/Tooltip/TooltipStepper";
+export {default as Tooltip} from './components/Tooltip/Tooltip';
+export {default as TooltipStepper} from './components/Tooltip/TooltipStepper';
 
-export { default as Image } from "./components/Image/Image";
-export { default as Toolbar } from "./components/Toolbar/Toolbar";
-export { default as SignatureCanvas } from "./components/Signature/SignatureCanvas";
-export { Modalize } from "./components/Modalize";
-export { default as Chip } from "./components/Chip/Chip";
-export { default as Slider } from "./components/Slider/Slider";
-export { default as RangeSlider } from "./components/Slider/RangeSlider";
-export { default as Accordion } from "./components/Accordion/Accordion";
+export {default as Image} from './components/Image/Image';
+export {default as Toolbar} from './components/Toolbar/Toolbar';
+export {default as SignatureCanvas} from './components/Signature/SignatureCanvas';
+export {Modalize} from './components/Modalize';
+export {default as Chip} from './components/Chip/Chip';
+export {default as Slider} from './components/Slider/Slider';
+export {default as RangeSlider} from './components/Slider/RangeSlider';
+export {default as Accordion} from './components/Accordion/Accordion';
